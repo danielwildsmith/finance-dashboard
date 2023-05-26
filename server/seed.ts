19d29823d5924d3ed10e3f85f415ac1a96de31a8
Daniel Wildsmith <danielwildsmith@ufl.edu>
2023-05-26 13:29:47 -0400

    feat: seed db

---
+++ server/seed.ts
@@ -0,0 +1,76 @@
+// Database syncronization script hosted on Firebase to execute daily
+// Also, when a new access_key is created, the db should be filled with all its data accordingly - separate func that takes in access_key
+import { UserToken } from "./models/user-token";
+import { Transaction } from "./models/transaction";
+import { Balance } from "./models/balance";
+import axios from 'axios';
+import { format } from 'date-fns';
+
+// Want to fill transactions and balance tables
+// For each access_key, there are transactions and balances needed to be added
+export const SeedDB = async () => {
+    // Get all access_keys (with username as well)
+    const user_tokens = await UserToken.findAll();
+    for(let i = 0; i < user_tokens.length; i++) {
+        // Make requests to Plaid API balances and transactions with each access_key
+
+        const req = {
+            access_token: user_tokens[i].dataValues.access_token
+        }
+        //@ts-ignore
+        axios.put('http://localhost:8000/api/transactions/all', req)
+            .then(res => {
+                //@ts-ignore
+                res.data.forEach(transaction => { 
+                    (async () => {
+                        await Transaction.findOrCreate(
+                            { 
+                                where: { transaction_id: transaction.transaction_id },
+                                defaults: 
+                                    {
+                                        date: transaction.date,
+                                        name: transaction.merchant_name,
+                                        category: JSON.stringify(transaction.category),
+                                        amount: transaction.amount,
+                                        username: user_tokens[i].dataValues.username
+                                    }
+                            }
+                        );
+                    })();
+                });
+            }) 
+            .catch(error => {
+                console.error(error);
+            });
+
+        axios.put('http://localhost:8000/api/balances/all', req)
+            .then(res => {
+                const currentDate = new Date();
+                const formattedDate : string = format(currentDate, 'yyyy-MM-dd');
+
+                //@ts-ignore
+                res.data.forEach(account => { 
+                    (async () => {
+                        await Balance.findOrCreate(
+                            { 
+                                where: { account_id: account.account_id, date: formattedDate},
+                                defaults: 
+                                    {
+                                        amount: account.balances.current,
+                                        account_name: account.name,
+                                        type: account.subtype,
+                                        username: user_tokens[i].dataValues.username
+                                    }
+                            }
+                        );
+                    })();
+                });
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+};
+
+
+
--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import apiRoute from './routes/api'
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import { db, ConnectDB } from './config';
+import { SeedDB } from './seed';
 
 const app : Express = express();
 const port = process.env.PORT || 8000;
@@ -12,6 +13,7 @@ const port = process.env.PORT || 8000;
 ConnectDB();
 
 // init middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieSession({
   name: "login-token",
@@ -29,3 +31,6 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+SeedDB();
+db.sync();
