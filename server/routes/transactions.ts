9231dc0522cfc2b2b0c753291877f6b9ffc09abe
Daniel Wildsmith <danielwildsmith@ufl.edu>
2023-06-05 18:29:51 -0400

    feat: route for monthly transactions comparisons

--- server/routes/transactions.ts
+++ server/routes/transactions.ts
@@ -3,7 +3,8 @@ import { plaidClient } from '../utils/config';
 import { TransactionsGetRequest } from 'plaid';
 import { format } from 'date-fns';
 import { Transaction } from '../models/transaction';
-import { Op } from 'sequelize';
+import { Op, Model } from 'sequelize';
+import { MonthlyCategoryData } from '../../src/components/transactions/transactions';
 
 const router = express.Router();
 
@@ -14,7 +15,8 @@ router.post('/', async function (req : Request, res : Response) {
   const transactionsReq: TransactionsGetRequest = {
     access_token: req.body.access_token,
     start_date: '2000-01-01',
-    end_date: formattedDate
+    end_date: formattedDate,
+    options: { include_personal_finance_category: true }
   };
 
   try {
@@ -44,10 +46,11 @@ router.get('/:username', async function (req : Request, res : Response) {
 });
 
 // For category: amount chart
-router.get('/category/:username/:yyyy/:mm', async function (req : Request, res : Response) {
+router.get('/categorized/:username/:yyyy/:mm', async function (req : Request, res : Response) {
   const username : string = req.params.username;
   const year : string = req.params.yyyy;
   const month : string = req.params.mm;
+  let days_in_month = new Date(parseInt(year), parseInt(month), 0).getDate();
 
   // find all transaction records associated with this username in this month on this year
   const transactions = await Transaction.findAll({
@@ -55,7 +58,7 @@ router.get('/category/:username/:yyyy/:mm', async function (req : Request, res :
       username: username,
       date: {
         [Op.gte]: `${year}-${month}-01`,
-        [Op.lte]: `${year}-${month}-31`
+        [Op.lte]: `${year}-${month}-${days_in_month}`
       }
     }
   });
@@ -86,11 +89,129 @@ router.get('/category/:username/:yyyy/:mm', async function (req : Request, res :
   res.status(200).send(formatted_category_amounts);
 });
 
+// for month to month comparisons with category breakdown
+router.get('/monthly/:username/:yyyy/:mm', async function (req : Request, res : Response) {
+  const username : string = req.params.username;
+  let year : string = req.params.yyyy;
+  const month : string = req.params.mm;
+  let days_in_month = new Date(parseInt(year), parseInt(month), 0).getDate();
+
+  // find all transaction records associated with this username in this month on this year
+  const selected_month_transactions = await Transaction.findAll({
+    where: {
+      username: username,
+      date: {
+        [Op.gte]: `${year}-${month}-01`,
+        [Op.lte]: `${year}-${month}-${days_in_month}`
+      }
+    }
+  });
+
+  // find all transaction records associated with this username in the previous and penultimate months
+  let previous_month = parseInt(month) - 1;
+  let penultimate_month = parseInt(month) - 2;
+  // edge case: January (00, -01 => 12, 11)
+  if(previous_month === 0 && penultimate_month === -1) {
+    previous_month = 12;
+    penultimate_month = 11;
+    year = (parseInt(year) - 1).toString();
+  }
+  const formatted_previous_month = previous_month.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+  days_in_month = new Date(parseInt(year), previous_month, 0).getDate();
+
+  const previous_month_transactions = await Transaction.findAll({
+    where: {
+      username: username,
+      date: {
+        [Op.gte]: `${year}-${formatted_previous_month}-01`,
+        [Op.lte]: `${year}-${formatted_previous_month}-${days_in_month}`
+      }
+    }
+  });
+
+  //edge case: February (01, 00 => 01, 12)
+  if(previous_month === 1 && penultimate_month === 0) {
+    penultimate_month = 12;
+    year = (parseInt(year) - 1).toString();
+  }
+  const formatted_penultimate_month = penultimate_month.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+  days_in_month = new Date(parseInt(year), penultimate_month, 0).getDate();
+
+  const penultimate_month_transactions = await Transaction.findAll({
+    where: {
+      username: username,
+      date: {
+        [Op.gte]: `${year}-${formatted_penultimate_month}-01`,
+        [Op.lte]: `${year}-${formatted_penultimate_month}-${days_in_month}`
+      }
+    }
+  });
+
+  if(selected_month_transactions.length === 0 && previous_month_transactions.length === 0 && penultimate_month_transactions.length === 0) {
+    res.status(404).send( {error: "No transaction records associated with this username in this time period"} );
+    return;
+  }
+
+  const getByCategoryData = (month_transactions : Model<any, any>[]): { [category: string]: number } => {
+    let category_amounts_map: { [category: string]: number } = {};
+    month_transactions.forEach(transaction => {
+      if (transaction.dataValues.category && transaction.dataValues.amount > 0) {
+        let category : string = JSON.parse(transaction.dataValues.category);
+        if(category_amounts_map[category]) {
+          category_amounts_map[category] = category_amounts_map[category] + transaction.dataValues.amount;
+        }
+        else {
+          category_amounts_map[category] = transaction.dataValues.amount;
+        }
+      }
+    });
+    return category_amounts_map;
+  };
+
+  // move data into objects for front-end
+  let monthly_category_data : MonthlyCategoryData[] = [];
+  for(const [key, value] of Object.entries(getByCategoryData(selected_month_transactions))) {
+    monthly_category_data.push({
+      category: key, selected_month: month, selected_month_amount: value, previous_month: formatted_previous_month, previous_month_amount: 0, 
+      penultimate_month: formatted_penultimate_month, penultimate_month_amount: 0
+    });
+  };
+  for(const [key, value] of Object.entries(getByCategoryData(previous_month_transactions))) {
+    let index = monthly_category_data.findIndex((obj => obj.category === key));
+    // category does not exist already
+    if(index === -1) {
+      monthly_category_data.push({
+        category: key, selected_month: month, selected_month_amount: 0, previous_month: formatted_previous_month, previous_month_amount: value, 
+        penultimate_month: formatted_penultimate_month, penultimate_month_amount: 0
+      });
+    } 
+    else {
+      monthly_category_data[index].previous_month_amount = value;
+    }
+  };
+  for(const [key, value] of Object.entries(getByCategoryData(penultimate_month_transactions))) {
+    let index = monthly_category_data.findIndex((obj => obj.category === key));
+    // category does not exist already
+    if(index === -1) {
+      monthly_category_data.push({
+        category: key, selected_month: month, selected_month_amount: 0, previous_month: formatted_previous_month, previous_month_amount: 0, 
+        penultimate_month: formatted_penultimate_month, penultimate_month_amount: value
+      });
+    } 
+    else {
+      monthly_category_data[index].penultimate_month_amount = value;
+    }
+  };
+
+  res.status(200).send(monthly_category_data);
+});
+
 
 router.get('/:username/:yyyy/:mm', async function (req : Request, res : Response) {
   const username : string = req.params.username;
   const year : string = req.params.yyyy;
   const month : string = req.params.mm;
+  let days_in_month = new Date(parseInt(year), parseInt(month), 0).getDate();
 
   // find all transaction records associated with this username in this month on this year
   const transactions = await Transaction.findAll({
@@ -98,7 +219,7 @@ router.get('/:username/:yyyy/:mm', async function (req : Request, res : Response
       username: username,
       date: {
         [Op.gte]: `${year}-${month}-01`,
-        [Op.lte]: `${year}-${month}-31`
+        [Op.lte]: `${year}-${month}-${days_in_month}`
       }
     }
   });
