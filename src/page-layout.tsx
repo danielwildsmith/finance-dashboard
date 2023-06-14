71a14649f7bef6841adca0a4aff820bca26d7814
Daniel Wildsmith <danielwildsmith@ufl.edu>
2023-06-14 18:31:37 -0400

    feat: add template for default page layout

---
+++ src/page-layout.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import MiniDrawer from "./components/sidebar"
+
+export const PageLayout = ({ ContentComponent, page, isLinked }: {ContentComponent: React.ElementType, page: string, isLinked: boolean}) => {
+    return (
+      <div style={{display: 'flex'}}>
+          <MiniDrawer page={page} isLinked={isLinked}/>
+          <div style={{ flexGrow: 1, padding: '16px' }}>
+            <div style={{ marginTop: '14vh' }}>
+                <ContentComponent />
+            </div>
+          </div>
+      </div>
+    ) 
+}
